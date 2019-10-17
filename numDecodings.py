d11047ad8292ffe2a3a5de366199902cb551577d
jyz0309 <38248129+jyz0309@users.noreply.github.com>
2019-10-17 14:52:39 +0800

    Leetcode 解码方法(递归，动态规划)

--- numDecodings.py
+++ numDecodings.py
@@ -1,11 +1,16 @@
+'''
+暴力递归不可取
 class Solution:
-# 暴力递归
     result = 0
     @classmethod
     def numDecodings(self, s: str) -> int:
         if s[0] == '0':
             return 0
-        for i in range(1, len(s) + 1):
+        if len(s) >= 3:
+            j = 3
+        else:
+            j = len(s)+1
+        for i in range(1, j):
             if int(s[0:i]) > 26:
                 continue
             else:
@@ -19,8 +24,36 @@ class Solution:
         if l == 0:
             self.result += 1
             return
-        for i in range(1, len(s) + 1):
+        if len(s) >= 3:
+            j = 3
+        else:
+            j = len(s)+1
+        for i in range(1, j):
             if int(s[0:i]) > 26:
                 continue
             else:
                 self.helper(l - i, s[i:len(s)])
+'''
+
+
+class Solution:
+    @classmethod
+    def numDecodings(self, s):
+        result = [0 for _ in range(len(s))]
+        if len(s) == 0 or len(s) == 1:
+            return "" if len(s) == 0 else 1
+        result[0] = 1
+        if int(s[0])*10+int(s[1])>26:
+            result[1] = 1
+        else:
+            result[1] = 2
+        for i in range(2, len(s)):
+            if 10*int(s[i-1])+int(s[i]) <= 26:
+                result[i] = result[i-1]+result[i-2]
+            else:
+                result[i] = result[i-1]
+        return result[-1]
+
+
+result = Solution.numDecodings("4757562545844617494555774581341211511296816786586787755257741178599337186486723247528324612117156948")
+print(result )
